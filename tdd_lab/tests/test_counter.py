d3303a016e569422fe96c35be5c5f6fcde4ed4db
ashley-arellano <134816375+ashley-arellano@users.noreply.github.com>
2025-02-04 20:48:34 -0800

    Update test_counter.py
    -Removed duplicate code

--- tdd_lab/tests/test_counter.py
+++ tdd_lab/tests/test_counter.py
@@ -1,50 +1,3 @@
-
-"""
-Name: Jose Alarcon, NSHE: 5005581810, CS472 GitHub Lab
-Description: This file gathers git commits on a specific project.
-It then prints this information for easy parsing to .txt file
-"""
-
-
-"""
-Test Cases for Counter Web Service
-
-Create a service that can keep a track of multiple counters
-- API must be RESTful - see the status.py file. Following these guidelines, you can make assumptions about
-how to call the web service and assert what it should return.
-- The endpoint should be called /counters
-- When creating a counter, you must specify the name in the path.
-- Duplicate names must return a conflict error code.
-- The service must be able to update a counter by name.
-- The service must be able to read the counter
-"""
-import pytest
-from src import app
-from src import status
-
-@pytest.fixture()
-def client():
-    """Fixture for Flask test client"""
-    return app.test_client()
-
-@pytest.mark.usefixtures("client")
-class TestCounterEndpoints:
-    """Test cases for Counter API"""
-
-    def test_create_counter(self, client):
-        """It should create a counter"""
-        result = client.post('/counters/foo')
-        assert result.status_code == status.HTTP_201_CREATED
-        assert result.json == {"foo": 0} 
-
-    def test_prevent_duplicate_counters(self, client):
-        # return "409 Conflict" if a duplicate counter is created
-        client.post('/counters/foo')  # counter
-        result = client.post('/counters/foo')
-
-        assert result.status_code == status.HTTP_409_CONFLICT
-        assert result.json == {"error": "Counter foo already exists"}
-
 """
 Test Cases for Counter Web Service
 
@@ -76,6 +29,19 @@ class TestCounterEndpoints:
         """It should create a counter"""
         result = client.post('/counters/foo')
         assert result.status_code == status.HTTP_201_CREATED
+
+    """
+    Name: Jose Alarcon, NSHE: 5005581810, CS472 GitHub Lab
+    Description: This file gathers git commits on a specific project.
+    It then prints this information for easy parsing to .txt file
+    """
+    def test_prevent_duplicate_counters(self, client):
+        # return "409 Conflict" if a duplicate counter is created
+        client.post('/counters/foo')  # counter
+        result = client.post('/counters/foo')
+
+        assert result.status_code == status.HTTP_409_CONFLICT
+        assert result.json == {"error": "Counter foo already exists"}
 
     # ===========================
     # Test: Test Increment Counter (PUT/counter/<name>)
