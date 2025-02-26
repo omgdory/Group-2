7e5dc531ccde9ab1a2e738172a07d914645ab2cf
omgdory <dorianakhavan@icloud.com>
2025-02-25 22:37:42 -0800

    Task 2.2 - Add code snippet (before asking for documentation)

---
+++ gen_ai/task2.2/DorianAkhavan_docstring_update.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSmoothFollow : MonoBehaviour
+{
+    // https://www.youtube.com/watch?v=MFQhpwc6cKE&t=59s
+    private Vector3 TargetFwd_Offset = new Vector3(15,0,0);
+    private Vector3 TargetBack_Offset = new Vector3(15,180,0);
+    
+    // speed at which camera will return to position
+    private float smoothSpeed = 10f;
+
+    private int cameraSelection = 0;
+    private int maxTargets = 2;
+
+    void FixedUpdate() {
+        HandleCamera();
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Tab)) {
+            if(cameraSelection >= maxTargets-1) {
+                cameraSelection = 0;
+                return;
+            }
+            cameraSelection++;
+        }
+    }
+
+    void HandleCamera() {
+        if(cameraSelection == 0) {
+            // Handle movement; Lerp between current and targetted position
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, CarManager.CamTarget_fwd.position, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+            // Handle rotation; multiply offset by target rotation
+            transform.rotation = CarManager.CamTarget_fwd.rotation * Quaternion.Euler(TargetFwd_Offset);
+        }
+        else if(cameraSelection == 1) {
+            // Handle movement; Lerp between current and targetted position
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, CarManager.CamTarget_back.position, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+            // Handle rotation; multiply offset by target rotation
+            transform.rotation = CarManager.CamTarget_back.rotation * Quaternion.Euler(TargetBack_Offset);
+        }
+    }
+}
